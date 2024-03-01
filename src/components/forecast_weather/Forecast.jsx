223ce1681122b17fa4ddb9215ba56ec97d03ae90
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-03-01 15:20:18 -0700

    edit forecast section, css

--- src/components/forecast_weather/Forecast.jsx
+++ src/components/forecast_weather/Forecast.jsx
@@ -1,27 +1,49 @@
 import './Forecast.scss'
 import { images } from '../../constains'
 
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+
 const Forecast = ({ forecast }) => {
-  console.log(forecast)
+  const indices = [1, 9, 17, 25, 32]
+  const fiveDaysForecase = indices.map((index) =>
+    forecast.list.slice(index - 1, index)
+  )
 
-  forecast.list.slice(0, 7).map((x) => {
-    console.log(x)
-  })
+  const date = new Date().getDay()
+  const forecast_days = weekdays
+    .slice(date, weekdays.length)
+    .concat(weekdays.slice(0, date))
 
-  const test = '09d'
+  console.log(fiveDaysForecase[0])
   return (
-    <div className='app__forecast-wrapper'>
-      <div className='app__forecast-container forecast-gradiant app__flex'>
-        <h1>Monday</h1>
-        <div className='forecast-temp app__flex'>
-          <h2>24</h2>
-          <p>°C</p>
+    <div className='app__forecast-wrapper app__flex'>
+      {fiveDaysForecase.map((forecast, index) => (
+        <div
+          className='app__forecast-container forecast-gradiant app__flex'
+          key={`forecast-${index}`}
+        >
+          <h1>{forecast_days[index]}</h1>
+          <div className='forecast-temp app__flex'>
+            <h2>{parseInt(forecast[0].main.temp)}</h2>
+            <p>°C</p>
+          </div>
+          <div className='forecast-weather app__flex'>
+            <img
+              src={images[`icon_${forecast[0].weather[0].icon}`]}
+              alt={forecast[0].weather[0].description}
+            />
+            <p>{forecast[0].weather[0].description}</p>
+          </div>
         </div>
-        <div className='forecast-weather app__flex'>
-          <img src={images[`icon_${test}`]} alt='' />
-          <p>Partly Cloudy</p>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
